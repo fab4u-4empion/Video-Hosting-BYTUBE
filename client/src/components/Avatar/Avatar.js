c28c0918ab300b929b1f8b426263c1197f2eb070
fab4u_4empion <reserv390@gmail.com>
2023-04-04 22:14:47 +0300

    [client] Refactor avatar component

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -3,36 +3,42 @@ import {Icon28UserOutline} from "@vkontakte/icons";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
-export const Avatar = ({src, size = 20, overlay}) => {
-    const [imgURL, setImgURL] = useState(null)
+export const Avatar = ({src, size = 20, overlay, dataURLContent = false}) => {
+    const [loaded, setLoaded] = useState(false)
+    const [failed, setFailed] = useState(false)
 
-    useEffect(() => {
-        axios
-            .get(src, {responseType: "blob"})
-            .then(response => {
-                const r = new FileReader()
-                r.readAsDataURL(response.data)
-                r.onload = () => {
-                    setImgURL(r.result)
-                }
-            })
-    }, [])
+    const needFallBack = failed || !src
+
+    const handleImageLoad = () => {
+        setLoaded(true)
+        setFailed(false)
+    }
+
+    const handleImageError = () => {
+        setLoaded(false)
+        setFailed(true)
+    }
 
     return (
-        <div className="avatar-container" style={{width: size, height: size}}>
+        <div
+            className={`avatar-container ${loaded && "avatar-loaded"}`}
+            style={{width: size, height: size}}
+        >
             {overlay &&
-                <div className="avatar-overlay">{overlay}</div>
+                <div className="avatar-overlay" onClick={overlay.action}>{overlay.icon}</div>
             }
-            {!imgURL &&
-                <div className="avatar-icon"><Icon28UserOutline width={size * 0.75} height={size * 0.75}/></div>
-            }
-            {imgURL &&
+            {src &&
                 <img
-                    src={imgURL}
+                    src={src}
                     className="avatar-img"
                     width={size}
                     height={size}
+                    onLoad={handleImageLoad}
+                    onError={handleImageError}
                 />
+            }
+            {needFallBack &&
+                <div className="avatar-fallback"><Icon28UserOutline width={size * 0.75} height={size * 0.75}/></div>
             }
         </div>
     )
