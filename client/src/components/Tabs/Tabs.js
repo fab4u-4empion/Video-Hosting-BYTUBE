9fff140101fe8fc1368f2ab11d7b660dc379b61a
fab4u_4empion <reserv390@gmail.com>
2023-06-10 13:23:51 +0300

    [client] Some fixes

--- client/src/components/Tabs/Tabs.js
+++ client/src/components/Tabs/Tabs.js
@@ -1,8 +1,9 @@
 import "./tabs.css"
+import {useCallback, useEffect, useRef, useState} from "react";
 
 export const Tabs = ({children, className}) => {
     return (
-        <div className={`tabs-container ${className}`}>
+        <div className={`tabs-container ${className ?? ""}`}>
             {children}
         </div>
     )
